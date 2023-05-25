b8143e355197b555a0d18f010583701a4e126743
AlaaNaserEasyApply <anaser@easyapplyedu.com>
2023-05-25 19:03:15 +0300

    install validator and transform pipes and set them as global pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (errors) =>
+        new BadRequestException(JSON.stringify(errors)),
+      whitelist: true,
+      transform: true,
+    }),
+  );
   await app.listen(process.env.LISTENING_PORT ?? 3000);
 }
 bootstrap();
