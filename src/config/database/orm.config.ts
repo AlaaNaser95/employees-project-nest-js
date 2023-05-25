fce16b35ead9c397e00016f504a1d08e1f717cc5
AlaaNaserEasyApply <anaser@easyapplyedu.com>
2023-05-25 18:57:55 +0300

    fix orm config

--- src/config/database/orm.config.ts
+++ src/config/database/orm.config.ts
@@ -6,14 +6,15 @@ const env = process.env;
 const ormConfig = {
   type: env.DATABASE_TYPE || 'mysql',
   host: env.DATABASE_HOST || 'localhost',
-  port: env.DATABASE_PORT || 3306,
+  port: parseInt(env.DATABASE_PORT) || 3306,
   username: env.DATABASE_USERNAME,
   password: env.DATABASE_PASSWORD,
   database: env.DATABASE,
   entities: ['dist/database/entities/*.js'],
-  synchronize: env.DATABASE_SYNC || false,
-  autoLoadEntities: true,
-  logging: env.DATABASE_LOGGING || false,
+  synchronize: env.DATABASE_SYNC === 'true' ? true : false,
+  logging: env.DATABASE_LOGGING === 'true' ? true : false,
+  migrationsTableName: 'migrations',
+  migrations: ['dist/database/migrations/*.js'],
 };
 
 export default ormConfig;
