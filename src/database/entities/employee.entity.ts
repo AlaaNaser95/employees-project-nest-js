9b9865882a021d9942fe9156a0337657bc58ebdf
AlaaNaserEasyApply <anaser@easyapplyedu.com>
2023-05-25 19:04:05 +0300

    Create employee entity

---
+++ src/database/entities/employee.entity.ts
@@ -0,0 +1,59 @@
+import { Column, Entity } from 'typeorm';
+import {
+  IsDate,
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+import { Base } from './base';
+import { PositionsEnum } from '../../enums/positions.enum';
+
+@Entity()
+export class Employee extends Base {
+  @Column()
+  @IsNotEmpty()
+  @IsString()
+  firstName: string;
+
+  @Column()
+  @IsNotEmpty()
+  @IsString()
+  lastName: string;
+
+  @Column()
+  @IsNotEmpty()
+  @IsString()
+  phoneCode: string;
+
+  @Column()
+  @IsNotEmpty()
+  @IsString()
+  phoneNumber: string;
+
+  @Column('date', { nullable: true })
+  @IsDate()
+  @IsOptional()
+  dob: Date;
+
+  @Column('date', { nullable: true })
+  @IsDate()
+  @IsOptional()
+  joiningDate: Date;
+
+  @Column()
+  @IsEnum(PositionsEnum)
+  @IsNotEmpty()
+  position: PositionsEnum;
+
+  @Column('int', { default: 0 })
+  @IsInt()
+  @IsNotEmpty()
+  absenceDaysCount: number;
+
+  @Column({ nullable: true })
+  @IsOptional()
+  @IsString()
+  address: string;
+}
---
+++ src/enums/positions.enum.ts
@@ -0,0 +1,8 @@
+export enum PositionsEnum {
+  MANAGER = 'manager',
+  BACKEND_DEVELOPER = 'backend developer',
+  FRONTEND_DEVELOPER = 'frontend developer',
+  CEO = 'CEO',
+  CTO = 'CTO',
+  OFFICE_BOY = 'office boy',
+}
