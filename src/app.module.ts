e7dd2537fa53cbd9f27001a0fc24076157702310
AlaaNaserEasyApply <anaser@easyapplyedu.com>
2023-05-25 16:40:24 +0300

    Setup config module with .env Setup typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { EmployeeModule } from './modules/employee/employee.module';
+import { ConfigModule } from '@nestjs/config';
+import DatabaseConfig from './config/database/database.config';
 
 @Module({
-  imports: [EmployeeModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [DatabaseConfig],
+    }),
+    EmployeeModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/config/database/orm.config.ts
@@ -0,0 +1,19 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const env = process.env;
+
+const ormConfig = {
+  type: env.DATABASE_TYPE || 'mysql',
+  host: env.DATABASE_HOST || 'localhost',
+  port: env.DATABASE_PORT || 3306,
+  username: env.DATABASE_USERNAME,
+  password: env.DATABASE_PASSWORD,
+  database: env.DATABASE,
+  entities: ['dist/database/entities/*.js'],
+  synchronize: env.DATABASE_SYNC || false,
+  autoLoadEntities: true,
+  logging: env.DATABASE_LOGGING || false,
+};
+
+export default ormConfig;
